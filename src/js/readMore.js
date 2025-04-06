fa999c7fd1a75afe94ab8752f169f459f00dcd22
Puneet <puneetkfl147@gmail.com>
2025-04-06 20:40:33 +0530

    9th commit

--- src/js/readMore.js
+++ src/js/readMore.js
@@ -1,8 +1,11 @@
 const heroSec2para = document.getElementById("heroSec2para");
-const readMoreHS2P = document.getElementById("readMoreHS2P")
+const readMoreHS2P = document.getElementById("readMoreHS2P");
 
-readMoreHS2P.addEventListener("click", toggleReadMore)
+readMoreHS2P.addEventListener("click", toggleReadMore);
 
-function toggleReadMore(){
-    heroSec2para.classList.toggle("text-clap-3")
+function toggleReadMore() {
+    // Toggle the class that controls visibility/truncation
+    heroSec2para.classList.toggle("text-clap-3");
+
+    readMoreHS2P.textContent = heroSec2para.classList.contains("text-clap-3") ? "Read More" : "Read Less"; // Change text
 }
