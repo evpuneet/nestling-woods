8142485513c21a10523a6a6f9bf542cc5b811f6d
Puneet <puneetkfl147@gmail.com>
2025-04-11 15:41:34 +0530

    52th commit

--- src/js/showHideProjectStatus.js
+++ src/js/showHideProjectStatus.js
@@ -1,6 +1,6 @@
 const button = document.getElementById('toggleButtonProjectStatus');
         const parentDiv = document.getElementById('parentDivProjectStatus');
-        const hiddenDivs = parentDiv.querySelectorAll('[data-index]:nth-child(n+13)');
+        const hiddenDivs = parentDiv.querySelectorAll('[data-index]:nth-child(n+12)');
         
         let isExpanded = false;
 
@@ -11,9 +11,15 @@ const button = document.getElementById('toggleButtonProjectStatus');
                 if (isExpanded) {
                     div.classList.remove('hidden');  // Show the div
                     div.classList.add('flex');       // Restore flex
+                    parentDiv.classList.add('h-[550px]');       // Restore flex
+                    parentDiv.classList.remove('h-[300px]');       // Restore flex
+                   
                 } else {
                     div.classList.remove('flex');    // Remove flex first
                     div.classList.add('hidden');     // Then hide
+                    parentDiv.classList.add('h-[300px]');       // Restore flex
+                    parentDiv.classList.remove('h-[550px]');       // Restore flex
+
                 }
             });
 
