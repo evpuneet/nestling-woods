0a691b0a14e3b43b6b5155e5537c56a26125566c
Puneet <puneetkfl147@gmail.com>
2025-04-14 00:37:21 +0530

    85th commit

--- src/js/lightHouseGallery.js
+++ src/js/lightHouseGallery.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
       $(this).attr("data-caption", $(this).find("img").attr("alt"));
       $(this).attr("title", $(this).find("img").attr("alt"));
     });
+    
     // start fancybox:
     $("#sample-flat-img a").fancybox();
   });
@@ -30,6 +31,46 @@ $(document).ready(function() {
       $(this).attr("data-caption", $(this).find("img").attr("alt"));
       $(this).attr("title", $(this).find("img").attr("alt"));
     });
+    
     // start fancybox:
     $("#construction-updates-img a").fancybox();
+  });
+
+
+  $(document).ready(function() {
+    // add all to same gallery
+    $("#sample-flat-mob a").attr("data-fancybox","mygallery");
+    // assign captions and title from alt-attributes of images:
+    $("#sample-flat-mob a").each(function(){
+      $(this).attr("data-caption", $(this).find("img").attr("alt"));
+      $(this).attr("title", $(this).find("img").attr("alt"));
+    });
+    
+    // start fancybox:
+    $("#sample-flat-mob a").fancybox();
+  });
+
+  $(document).ready(function() {
+    // add all to same gallery
+    $("#project-gallery-mob a").attr("data-fancybox","mygallery");
+    // assign captions and title from alt-attributes of images:
+    $("#project-gallery-mob a").each(function(){
+      $(this).attr("data-caption", $(this).find("img").attr("alt"));
+      $(this).attr("title", $(this).find("img").attr("alt"));
+    });
+    // start fancybox:
+    $("#project-gallery-mob a").fancybox();
+  });
+
+  $(document).ready(function() {
+    // add all to same gallery
+    $("#construction-updates-mob a").attr("data-fancybox","mygallery");
+    // assign captions and title from alt-attributes of images:
+    $("#construction-updates-mob a").each(function(){
+      $(this).attr("data-caption", $(this).find("img").attr("alt"));
+      $(this).attr("title", $(this).find("img").attr("alt"));
+    });
+    
+    // start fancybox:
+    $("#construction-updates-mob a").fancybox();
   });
